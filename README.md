90059d5a9c673f87caad7c3b9fde388101b1017d
Chris Bielinski <chrisb@users.noreply.github.com>
2019-06-14 10:47:08 -0700

    update readme

--- README.md
+++ README.md
@@ -16,7 +16,19 @@ Copy the sample `.env`:
 cp .env.example .env
 ```
 
-Edit `.env` with your Officevibe auth token (once logged in, use the value of the `officevibe.auth` cookie), Slack token, and Slack channel ID.
+Now, edit `.env` with your credentials. You will need:
+
+#### `OFFICEVIBE_AUTH`
+
+To get your auth token log in to the Officevibe app and copy the value of the `officevibe.auth` cookie; this can be done through the Developer Tools of your browser or any number of browser extensions (like [EditThisCookie](https://chrome.google.com/webstore/detail/editthiscookie/fngmhnnpilhplaeedifhccceomclgfbg?hl=en) for Chrome).
+
+#### `SLACK_API_TOKEN`
+
+You'll need to set up a [Slack app](https://api.slack.com/apps) and use the **OAuth Access Token** you are provisioned.
+
+#### `SLACK_CHANNEL_ID`
+
+Find the ID of the channel you want to post results to using the Slack web client (check the URL) or use the API to get the ID.
 
 ### Usage
 
--- lib/officevibe/client.rb
+++ lib/officevibe/client.rb
@@ -1,26 +1,33 @@
+# frozen_string_literal: true
+
 module Officevibe
   class Client
     attr_reader :auth_token, :connection
 
     delegate :get, to: :connection
 
+    OFFICEVIBE_URL = "https://app.officevibe.com"
+    MANAGEABLE_GROUPS_PATH = "/groups/manageable"
+    ENGAGEMENT_REPORT_PATH = "/report/engagement"
+    AUTH_TOKEN_COOKIE = "officevibe.auth"
+
     def initialize(auth_token)
       @auth_token = auth_token
-      @connection = Faraday.new url: "https://app.officevibe.com", headers: headers do |conn|
+      @connection = Faraday.new url: OFFICEVIBE_URL, headers: headers do |conn|
         conn.response :json, :content_type => /\bjson$/
         conn.adapter Faraday.default_adapter
       end
     end
 
     def get_groups
-      get("/groups/manageable")
+      get(MANAGEABLE_GROUPS_PATH)
         .body["result"]
         .map { |obj| obj.deep_transform_keys(&:underscore) }
         .map(&:with_indifferent_access)
     end
 
     def get_engagement_report(group_id)
-      get("/report/engagement", groupId: group_id)
+      get(ENGAGEMENT_REPORT_PATH, groupId: group_id)
         .body["data"]
         .deep_transform_keys(&:underscore)
         .with_indifferent_access
@@ -29,7 +36,7 @@ module Officevibe
     private
 
     def headers
-      { "Cookie" => { "officevibe.auth" => auth_token }.to_query }
+      { "Cookie" => { AUTH_TOKEN_COOKIE => auth_token }.to_query }
     end
 
   end
